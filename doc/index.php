51dd986c0d6e2d7188038e045769d0f85c8a64cd
Marcos Paulo da Silva <mpsacademico@gmail.com>
2017-02-21 14:41:35 -0300

    Criação da estrutura básica da documentação

---
+++ doc/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+	<head>
+	 	<meta charset="UTF-8">
+		<title>Documentação MASPT</title>
+		<link rel="stylesheet" type="text/css" href="estilo.css">
+	</head>
+	<body>
+	<h1>MASPT</h1>
+	<h2>Documentação</h2>
+	<h3>Previsão do Tempo</h3>
+	<h4>Previsão completa para uma localidade</h4>
+	</body>
+</html> 
