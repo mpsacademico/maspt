0e63ead3591730f3d8000b4823ab963c5ecf7418
Marcos Paulo da Silva <mpsacademico@gmail.com>
2017-02-24 08:09:11 -0300

    Estruturação da verificação dos dados de entrada

--- api/previsao.php
+++ api/previsao.php
@@ -6,37 +6,42 @@ $cl = isset($_GET['cl']) ? $_GET['cl'] : NULL; //código da localidade
 $tr = isset($_GET['tr']) ? $_GET['tr'] : NULL; //tipo de retorno
 $ca = isset($_GET['ca']) ? $_GET['ca'] : NULL; //chave de acesso
 
-require_once("src/autenticacao_classe.php");
-$aut = new Autenticacao($ca);
+//require_once("src/autenticacao_classe.php");
+//$aut = new Autenticacao($ca);
+
+if(!empty($ca)) {
+	//buscar no banco de dados se a chave existe e está válida
+	if(true){
+	}else{
+		die();
+	}		
+}else{
+	$erro = 300; //a chave obrigatória não foi informada
+	die();
+}
 
 if(is_null($tr) || empty($tr)){
 	$tr = "json";
 }else{
 	$tr = strtolower($tr);
 	if(!in_array($tr, $trs)){
-		echo "de";
-		die;
+		$tr = "json";
 	}
 }
 
-if(!is_null($cl)){
-$dados = file_get_contents("http://servicos.cptec.inpe.br/apptempo/".$cl);
-	if(!is_null($tr)){	
-		if(strcmp($tr,"xml")==0){
-			header("Content-type: text/xml");
-			echo $dados;
-		}elseif(strcmp($tr,"json")==0){
-			header("Content-type: application/json");
-			$xml = simplexml_load_string($dados);
-			$json = json_encode($xml);
-			echo $json;
-		}
-	}else{
-
-	}
-}
-else{
+if(is_null($cl)){	
 	echo "Nenhuma localidade informada!";
+	die();
 }
 
+$dados = file_get_contents("http://servicos.cptec.inpe.br/apptempo/".$cl);
+if(strcmp($tr,"xml")==0){
+	header("Content-type: text/xml");
+	echo $dados;
+}elseif(strcmp($tr,"json")==0){
+	header("Content-type: application/json");
+	$xml = simplexml_load_string($dados);
+	$json = json_encode($xml);
+	echo $json;
+}
 ?>
