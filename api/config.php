66da1708cd86ae354d5a4b6e3e820604f4054f7a
Marcos Paulo da Silva <mpsacademico@gmail.com>
2017-03-13 08:10:17 -0300

    Desligamento de reporte de erros fora do debug

--- api/config.php
+++ api/config.php
@@ -1,9 +1,11 @@
 <?php
-	define("DEBUG", true);
+	define("DEBUG", false);
 
 	if(DEBUG == true) {
 		ini_set('display_errors', 1);
 		ini_set('display_startup_errors', 1);
 		error_reporting(E_ALL);
+	}else{
+		error_reporting(0);
 	}
 ?>
